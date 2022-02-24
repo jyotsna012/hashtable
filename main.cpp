70b7a13b52d6eb15782df1c5ca61ce4abaf7bb69
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-24 11:23:52 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(){
   
   int index;
-  char name[] = "Hello"; 
+  char name[] = "Hellos"; 
   hashmap hashObj;
   index = hashObj.Hash(name);
   cout <<"index: " << index;
