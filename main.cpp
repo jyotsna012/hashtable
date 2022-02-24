5142b011a5b3b5bf15e739addf095f922272b603
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-24 11:17:09 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+
+#include "hash.h"
+
+using namespace std;
+
+int main{
+  
+  char name[] = "Hello"; 
+  has hashObj;
+  index = hashObj.Hash(name);
+  
+  cout <<"index: " << index;
+
+}
+
 
