384aa6ccc61883f54f2562ff42caea65745b8825
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-24 11:40:48 -0800

    Update hash.h

--- hash.h
+++ hash.h
@@ -7,7 +7,17 @@ using namespace std;
 class hashmap{
 
   private:
-    int tableSize;
+    static const int tableSize = 10;
+    struct Student {
+      char firstName[20];
+      char lastName[20];
+      int studentId;
+      float GPA;
+      student* next;
+    };
+  
+    item* HashTable[tableSize];
+  
   public:
     hashmap();
     int Hash(char* key);
