79b2bef9ee46feedb7b82870a8e2953210cf49e2
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-24 11:17:15 -0800

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,1 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
 
+#include "hash.h"
+
+using namespace std;
+
+int hash::Hash(char* key){
+  int hash = 0;
+  int index;
+  
+  index = strlen(key);
+
+  return index;
+  
+}
